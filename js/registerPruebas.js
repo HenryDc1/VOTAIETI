4926d880b58200ee3603adffa9e5f11a8599b540
arnaumv <95907269+arnaumv@users.noreply.github.com>
2024-01-26 11:45:31 +0100

    Add files via upload

--- js/registerPruebas.js
+++ js/registerPruebas.js
@@ -18,14 +18,16 @@ $(document).ready(function() {
 
             // Validar que el nombre de usuario tenga al menos 5 caracteres y contenga mayúsculas y minúsculas
             if (username.length >= 5 && username != username.toLowerCase() && username != username.toUpperCase()) {
-                // Si la validación es exitosa, agregar el campo de entrada del correo electrónico
-                form.append('<div class="datosUsuarioRegister">' +
-                            '<input class="inputRegisterPHP" type="email" id="email" name="email" required>' +
-                            '<label for="email">Correo electrónico</label>' +
-                        '</div>');
-
-                // Mover el foco al campo de entrada del correo electrónico
-                $('#email').focus();
+                // Si la validación es exitosa y el campo de entrada del correo electrónico no existe, agregarlo
+                if (!$("#email").length) {
+                    form.append('<div class="datosUsuarioRegister">' +
+                                '<input class="inputRegisterPHP" type="email" id="email" name="email" required>' +
+                                '<label for="email">Correo electrónico</label>' +
+                            '</div>');
+            
+                    // Mover el foco al campo de entrada del correo electrónico
+                    $('#email').focus();
+                }
 
                 // Manejar el evento keydown en el campo de entrada del correo electrónico
                 form.on('keydown', '#email', function(e) {
@@ -40,14 +42,16 @@ $(document).ready(function() {
                         // Validar que el correo electrónico tenga el formato correcto
                         var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
                         if (emailRegex.test(email)) {
-                            // Si la validación es exitosa, agregar el campo de entrada de la contraseña
-                            form.append('<div class="datosUsuarioRegister">' +
-                                        '<input class="inputRegisterPHP" type="password" id="password" name="password" required>' +
-                                        '<label for="password">Contraseña</label>' +
-                                    '</div>');
-
-                            // Mover el foco al campo de entrada de la contraseña
-                            $('#password').focus();
+                            // Si la validación es exitosa y el campo de entrada de la contraseña no existe, agregarlo
+                            if (!$("#password").length) {
+                                form.append('<div class="datosUsuarioRegister">' +
+                                            '<input class="inputRegisterPHP" type="password" id="password" name="password" required>' +
+                                            '<label for="password">Contraseña</label>' +
+                                        '</div>');
+                        
+                                // Mover el foco al campo de entrada de la contraseña
+                                $('#password').focus();
+                            }
 
                             // Manejar el evento keydown en el campo de entrada de la contraseña
                             form.on('keydown', '#password', function(e) {
@@ -65,10 +69,13 @@ $(document).ready(function() {
                                     } else if (!/[!@#$%^&*]/.test(password)) {
                                         showErrorPopup('La contraseña debe contener al menos un carácter especial.');
                                     } else {
-                                        form.append('<div class="datosUsuarioRegister">' +
-                                                    '<input class="inputRegisterPHP" type="password" id="confirmPassword" name="confirm_password" required>' +
-                                                    '<label for="confirmPassword">Confirmar contraseña</label>' +
-                                                '</div>');
+                                        // Si la contraseña confirmada es la misma que la contraseña original, agrega el campo de confirmación de contraseña
+                                            if (!$("#confirmPassword").length) {
+                                                form.append('<div class="datosUsuarioRegister">' +
+                                                            '<input class="inputRegisterPHP" type="password" id="confirmPassword" name="confirm_password" required>' +
+                                                            '<label for="confirmPassword">Confirmar contraseña</label>' +
+                                                        '</div>');
+                                            }
                                                 setTimeout(function() {
                                                     $('#confirmPassword').focus();
                                                 }, 0);
@@ -81,19 +88,27 @@ $(document).ready(function() {
                                                         if (confirmPassword !== password) {
                                                             showErrorPopup('Las contraseñas no coinciden.');
                                                         } else {
-                                                            // Si la contraseña confirmada es la misma que la contraseña original, agrega el campo de selección de país
-                                                            form.append(countrySelectHTML);
-                                                            setTimeout(function() {
-                                                                $('#country').focus();
-                                                            }, 0);
-                                                            $('#country').change(function() {
-                                                                // Agrega el campo de número de teléfono
+                                                            // Si la contraseña confirmada es la misma que la contraseña original
+                                                        if (password === confirmPassword) {
+                                                            // Si el campo de selección de país no existe, agregarlo
+                                                            if (!$("#country").length) {
+                                                                form.append(countrySelectHTML);
+                                                            }
+                                                        }
+                                                        setTimeout(function() {
+                                                            $('#country').focus();
+                                                        }, 0);
+                                                        $('#country').change(function() {
+                                                            // Si el campo de número de teléfono no existe, agregarlo
+                                                            if (!$("#telephone").length) {
                                                                 form.append('<div class="datosUsuarioRegister">' +
                                                                             '<div style="display: flex; justify-content: space-between;">' +
                                                                             '<input class="inputRegisterPHP" type="tel" id="countryPrefix" name="countryPrefix" readonly style="margin-right: 10px; width: 15%;">' +
                                                                             '<input class="inputRegisterPHP" type="tel" id="telephone" name="telephone" required style="width: 80%;" placeholder="Número de teléfono">' +
                                                                             '</div>' +
                                                                             '</div>');
+                                                            }
+
                                                 
                                                                 // Obtiene el prefijo del país seleccionado
                                                                 var selectedCountryPrefix = $('#country option:selected').data('prefix');
@@ -123,13 +138,16 @@ $(document).ready(function() {
                                                                             return;
                                                                         }
                                                                 
-                                                                        // If the phone number is valid, add the city input field
+                                                                        /// If the phone number is valid
                                                                         if (telephoneWithoutPrefix.length === 9 && /^\d+$/.test(telephoneWithoutPrefix)) {
-                                                                            form.append('<div class="datosUsuarioRegister">' +
-                                                                                        '<input class="inputRegisterPHP" type="text" id="city" name="city" required>' +
-                                                                                        '<label for="city">Ciudad</label>' +
-                                                                                        '</div>');
-                                                                        
+                                                                            // If the city input field does not exist, add it
+                                                                            if (!$("#city").length) {
+                                                                                form.append('<div class="datosUsuarioRegister">' +
+                                                                                            '<input class="inputRegisterPHP" type="text" id="city" name="city" required>' +
+                                                                                            '<label for="city">Ciudad</label>' +
+                                                                                            '</div>');
+                                                                            }
+
                                                                             // Aquí es donde actualizas el valor del campo de teléfono en tu formulario HTML
                                                                             // para que sea el número de teléfono completo (incluyendo el prefijo).
                                                                             $('#telephone').val(telephone);
@@ -145,11 +163,13 @@ $(document).ready(function() {
                                                                 
                                                                         // Check if the city field is not empty
                                                                         if (city !== '') {
-                                                                            // Add the zipcode input field
-                                                                            form.append('<div class="datosUsuarioRegister">' +
-                                                                                        '<input class="inputRegisterPHP" type="text" pattern="[0-9]{5}" id="zipcode" name="zipcode" required>' +
-                                                                                        '<label for="zipcode">Código postal</label>' +
-                                                                                        '</div>');
+                                                                            // If the zipcode input field does not exist, add it
+                                                                            if (!$("#zipcode").length) {
+                                                                                form.append('<div class="datosUsuarioRegister">' +
+                                                                                            '<input class="inputRegisterPHP" type="text" pattern="[0-9]{5}" id="zipcode" name="zipcode" required>' +
+                                                                                            '<label for="zipcode">Código postal</label>' +
+                                                                                            '</div>');
+                                                                            }
                                                                         } else {
                                                                             // Show an error message
                                                                             showErrorPopup('Por favor, introduce una ciudad.');
