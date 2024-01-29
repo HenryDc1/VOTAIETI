e01a7932809072f89fc3ac68f3a4cf54e0fa2be3
arnaumv <95907269+arnaumv@users.noreply.github.com>
2024-01-29 10:18:14 +0100

    Update log_function.php

--- logs/log_function.php
+++ logs/log_function.php
@@ -1,9 +1,16 @@
 <?php
 function custom_log($event, $details) {
-    $date = date('Y-m-d');
-    $logFile = "../logs/log-$date.txt";
-    $current_time = date('H:i:s');
-    $formattedMessage = "[$date $current_time] $event: $details\n";
-    error_log($formattedMessage, 3, $logFile);
+    // Crear el nombre del archivo con el formato "log_aaaa_mm_dd.txt"
+    $logFile = "logs/log_" . date("Y-m-d") . ".txt";
+
+    // Asegurarse de que la carpeta "Logs" exista, si no, crearla
+    if (!is_dir("logs")) {
+        mkdir("logs");
+    }
+
+    // Escribir en el archivo de registro
+    $current_time = date("Y-m-d H:i:s");
+    $formattedMessage = "$current_time - $event: $details\n";
+    file_put_contents($logFile, $formattedMessage, FILE_APPEND);
 }
 ?>
