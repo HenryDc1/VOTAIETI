211e3315c4a41ed09ea44d625cd1c0666661d0b9
arnaumv <95907269+arnaumv@users.noreply.github.com>
2024-01-31 20:41:45 +0100

    Update register.php

--- register.php
+++ register.php
@@ -9,7 +9,7 @@ include 'log_function.php';
 
 // Correo electrónico del remitente (hardcodeado)
 $senderEmail = "amestrevizcaino.cf@iesesteveterradas.cat";
-$passwordEmail = "ArnauMestre169";
+$passwordEmail = "";
 
 // Muestra el mensaje de error si existe
 if (isset($_SESSION['error'])) {
