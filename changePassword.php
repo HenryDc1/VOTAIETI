d06cc8b87074aaba1a5bbe444114ad00933f6541
arnaumv <arnaumestre02@gmail.com>
2024-02-07 20:37:52 +0100

    aaa

--- changePassword.php
+++ changePassword.php
@@ -95,66 +95,66 @@
         <?php include 'footer.php'; ?>
 
         <script>
-    $(document).ready(function() {
-        // Función para verificar la contraseña
-        function checkPassword(password) {
-            // Debe tener al menos 8 caracteres, una letra mayúscula, una letra minúscula y un carácter especial
-            var regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-            return regex.test(password);
-        }
+                $(document).ready(function() {
+                    // Función para verificar la contraseña
+                    function checkPassword(password) {
+                        // Debe tener al menos 8 caracteres, una letra mayúscula, una letra minúscula y un carácter especial
+                        var regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+                        return regex.test(password);
+                    }
 
-        // Función para mostrar los mensajes de error
-        function showErrorPopup(message) {
-            alert(message);
-        }
+                    // Función para mostrar los mensajes de error
+                    function showErrorPopup(message) {
+                        alert(message);
+                    }
 
-        // Evitar que el formulario se envíe cuando se presiona Enter
-        $('.iniciasesionLogin').on('keydown', function(e) {
-            if (e.keyCode == 13) {
-                e.preventDefault();
-                return false;
-            }
-        });
+                    // Evitar que el formulario se envíe cuando se presiona Enter
+                    $('.iniciasesionLogin').on('keydown', function(e) {
+                        if (e.keyCode == 13) {
+                            e.preventDefault();
+                            return false;
+                        }
+                    });
 
-        // Cuando se presiona la tecla Enter o Tab en el campo de contraseña
-$('#past_Password').on('keyup', function(e) {
-    if (e.keyCode == 13 || e.keyCode == 9) { // 13 es Enter, 9 es Tab
-        if (checkPassword($(this).val())) {
-            // Si la contraseña es válida, agregar el campo de confirmación de contraseña
-            $('#password_div').html('<input class="inputLoginPHP" type="password" id="password" name="password" required><label for="password">Nueva Contraseña</label>');
-        } else {
-            // Si la contraseña no es válida, mostrar un mensaje de error
-            showErrorPopup('La contraseña debe tener al menos 8 caracteres, una letra mayúscula, una letra minúscula y un carácter especial.');
-        }
-    }
-});
+                    // Cuando se presiona la tecla Enter o Tab en el campo de contraseña
+            $('#past_Password').on('keyup', function(e) {
+                if (e.keyCode == 13 || e.keyCode == 9) { // 13 es Enter, 9 es Tab
+                    if (checkPassword($(this).val())) {
+                        // Si la contraseña es válida, agregar el campo de confirmación de contraseña
+                        $('#password_div').html('<input class="inputLoginPHP" type="password" id="password" name="password" required><label for="password">Nueva Contraseña</label>');
+                    } else {
+                        // Si la contraseña no es válida, mostrar un mensaje de error
+                        showErrorPopup('La contraseña debe tener al menos 8 caracteres, una letra mayúscula, una letra minúscula y un carácter especial.');
+                    }
+                }
+            });
 
-// Cuando se presiona la tecla Enter o Tab en el campo de contraseña
-$(document).on('keyup', '#password', function(e) {
-    if (e.keyCode == 13 || e.keyCode == 9) { // 13 es Enter, 9 es Tab
-        if (checkPassword($(this).val())) {
-            // Si la contraseña es válida, agregar el campo de confirmación de contraseña
-            $('#confirm_password_div').html('<input class="inputLoginPHP" type="password" id="confirm_password" name="confirm_password" required><label for="confirm_password">Confirmar Contraseña</label>');
-        } else {
-            // Si la contraseña no es válida, mostrar un mensaje de error
-            showErrorPopup('La contraseña debe tener al menos 8 caracteres, una letra mayúscula, una letra minúscula y un carácter especial.');
-        }
-    }
-});
+            // Cuando se presiona la tecla Enter o Tab en el campo de contraseña
+            $(document).on('keyup', '#password', function(e) {
+                if (e.keyCode == 13 || e.keyCode == 9) { // 13 es Enter, 9 es Tab
+                    if (checkPassword($(this).val())) {
+                        // Si la contraseña es válida, agregar el campo de confirmación de contraseña
+                        $('#confirm_password_div').html('<input class="inputLoginPHP" type="password" id="confirm_password" name="confirm_password" required><label for="confirm_password">Confirmar Contraseña</label>');
+                    } else {
+                        // Si la contraseña no es válida, mostrar un mensaje de error
+                        showErrorPopup('La contraseña debe tener al menos 8 caracteres, una letra mayúscula, una letra minúscula y un carácter especial.');
+                    }
+                }
+            });
 
-// Cuando se presiona la tecla Enter o Tab en el campo de confirmación de contraseña
-$(document).on('keyup', '#confirm_password', function(e) {
-    if (e.keyCode == 13 || e.keyCode == 9) { // 13 es Enter, 9 es Tab
-        if ($(this).val() == $('#password').val()) {
-            // Si las contraseñas coinciden, agregar el botón de envío
-            $('#submit_button_div').html('<button id="siguienteBotonLogin" type="submit">Restablecer Contraseña</button>');
-        } else {
-            // Si las contraseñas no coinciden, mostrar un mensaje de error
-            showErrorPopup('Las contraseñas no coinciden.');
-        }
-    }
-});
-    });
-</script>
+            // Cuando se presiona la tecla Enter o Tab en el campo de confirmación de contraseña
+            $(document).on('keyup', '#confirm_password', function(e) {
+                if (e.keyCode == 13 || e.keyCode == 9) { // 13 es Enter, 9 es Tab
+                    if ($(this).val() == $('#password').val()) {
+                        // Si las contraseñas coinciden, agregar el botón de envío
+                        $('#submit_button_div').html('<button id="siguienteBotonLogin" type="submit">Restablecer Contraseña</button>');
+                    } else {
+                        // Si las contraseñas no coinciden, mostrar un mensaje de error
+                        showErrorPopup('Las contraseñas no coinciden.');
+                    }
+                }
+            });
+                });
+            </script>
     </body>
 </html>
